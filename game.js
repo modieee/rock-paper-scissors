e1fe0ae82eb21989a3000a58cfa8533016ef0091
Modadeoluwa <modadegospel@gmail.com>
2024-04-12 18:29:27 +0100

    Added function to make the game loop

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
+// Global Variable
+// player and computer scores
+let playerScore;
+let computerScore;
 
-
-// Second get player input or choice of rock paper scissors.
+// get player input or choice of rock paper scissors.
 const playerSelection = prompt('rock, paper or scissors?')
 
 // computer choice input of rock paper scissors
@@ -8,34 +11,27 @@ function getComputerChoice (){
     const choices = [ 'rock', 'paper', 'scissors'];
     
     let randomIndex = Math.floor(Math.random() * choices.length);
-
-    let compChoice = choices[randomIndex];
-
-    return compChoice;
+    return randomIndex;
 }
 
-// function getTotalScore() {
-    // let playerScore = 0;
-    // let compScore = 0;
-// }
+// computer choice of rock paper scissors stored in the compChoice variable
+let compChoice = getComputerChoice();
+
 
 // Game Logic
 function playRound(getComputerChoice, playerSelection) {
-    let compChoice = getComputerChoice();
-    let playerScore = 0;
-    let compScore = 0;
 
     if (playerSelection == 'rock') {
         if (compChoice == 'paper') {
+            computerScore += 1;
             console.log('You chose: ', playerSelection);
             console.log('Computer chose: ', compChoice);
             console.log("You Lose!, Paper beats Rock!");
-            compScore += 1;
         } else if (compChoice == 'scissors') {
+            playerScore += 1;
             console.log('You chose: ', playerSelection);
             console.log('Computer chose: ', compChoice);
             console.log("You Win!, Rock beats Scissors!");
-            playerScore += 1;
         } else {
             console.log('You chose: ', playerSelection)
             console.log('Computer chose: ', compChoice)
@@ -43,15 +39,15 @@ function playRound(getComputerChoice, playerSelection) {
         }
     } else if (playerSelection == 'paper') {
         if (compChoice == 'rock') {
+            playerScore += 1;
             console.log('You chose: ', playerSelection)
             console.log('Computer chose: ', compChoice)
             console.log("You Win!, Paper beats Rock!")
-            playerScore += 1;
         } else if (compChoice == 'scissors') {
+            computerScore += 1;
             console.log('You chose: ', playerSelection)
             console.log('Computer chose: ', compChoice)
             console.log("You Lose!, Scissors cuts Paper!")
-            compScore += 1;
         } else {
             console.log('You chose: ', playerSelection)
             console.log('Computer chose: ', compChoice)
@@ -59,34 +55,38 @@ function playRound(getComputerChoice, playerSelection) {
         }
     } else if (playerSelection == 'scissors') {
         if (compChoice == 'rock') {
+            computerScore += 1;
             console.log('You chose: ', playerSelection);
             console.log('Computer chose: ', compChoice);
             console.log('You Lose!, Rock beats Scissors!');
-            compScore += 1;
         } else if (compChoice == 'paper') {
+            playerScore += 1;
             console.log('You chose: ', playerSelection);
             console.log('Computer chose: ', compChoice);
             console.log('You Win!, Scissors cuts Paper!');
-            playerScore += 1;
         } else {
             console.log('You chose: ', playerSelection)
             console.log('Computer chose: ', compChoice)
             console.log("Draw!, Scissors can't cut Scissors!")
         }
     }
-    console.log("Computer TotalScore:", compScore);
-    console.log("Your Total point is:", playerScore);
 }
 
-playRound(getComputerChoice, playerSelection)
+//  Game Run
+function playGame (times) {
+    if (computerScore <= 0 && playerScore <= 0) {
+        for (let i = 1; i <= times; i++) {
+            playRound(getComputerChoice, playerSelection);
+        }
+    } else if (computerScore == 3){
+        console.log("Game Over Computer Wins!");
+        console.log("Computer Score: " + computerScore)
+        console.log("Player Score: " + playerScore)
+    } else if (playerScore == 3){
+        console.log("You've Won!");
+        console.log("Player Score: " + playerScore)
+        console.log("Computer Score: " + computerScore)
+    }
+};
 
-
-// function playGame() {
-//     playRound(getComputerChoice, playerSelection);
-//     for (let i = 5; i < 1; i--) {
-//         console.log(playerScore[i]);
-//         console.log(compScore[i]);
-//     }
-// }
-
-// playGame();
+playGame(5);
